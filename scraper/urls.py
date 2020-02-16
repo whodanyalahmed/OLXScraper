1d735dfb5d8878877a5709be18c97550e8a2f0f4
Daniyal Ahmed <daniahmedkhatri@gmail.com>
2020-02-16 12:50:08 +0500

    Max and Min feature added

--- scraper/urls.py
+++ scraper/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import homeView,search
+from .views import homeView,search,minView,maxView,listView,test
 urlpatterns = [
     path('',homeView.as_view(),name="home"),
-    path('search',search,name="search")
+    path('search',search,name="search"),
+    path('min',minView.as_view(),name="min"),
+    path('max',maxView.as_view(),name="max"),
+    path('list',listView.as_view(),name="list"),
+    path('test',test,name="test"),
+    
+
     ]
--- scraper/views.py
+++ scraper/views.py
@@ -9,10 +9,21 @@ from bs4 import BeautifulSoup
 class homeView(TemplateView):
     template_name = "index.html"
 
+class minView(TemplateView):
+    template_name = "min.html"
+
+
+class maxView(TemplateView):
+    template_name = "max.html"
+
+
+class listView(TemplateView):
+    template_name = "list.html"
 
 
 def search(request):
     url = request.POST["url"]
+    title = request.POST.get('title')
     # url = request.POST.get('url',None)
     # con = Person.objects.get(name = url)
     try:    
@@ -64,10 +75,71 @@ def search(request):
 
         minsrc = link[mincount]
         maxsrc = link[maxcount]
-        return render(request,"search.html",context={'html':html,'min_name':minname,'min_price':minprice,'max_name':maxname,'max_price':maxprice,'minsrc':minsrc,'maxsrc':maxsrc})
+        return render(request,"search.html",context={'title':title,'html':html,'min_name':minname,'min_price':minprice,'max_name':maxname,'max_price':maxprice,'minsrc':minsrc,'maxsrc':maxsrc})
     except Exception as e:
         return render(request,"notfound.html",{'error':e})
 
+
+def test(request):
+    title = request.POST.get('title')
+    return HttpResponse(title)
+
+# def minima(request):
+#     url = request.POST["url"]
+#     # url = request.POST.get('url',None)
+#     # con = Person.objects.get(name = url)
+#     try:    
+#         html = requests.get("https://www.olx.com.pk/items/q-"+url)
+#         scode = html.status_code
+#         # print(html.status_code)
+
+#         soup = BeautifulSoup(html.text,"lxml")
+#         name =  []
+
+#         for each_name in soup.findAll('span',{'class': '_2tW1I'}):
+#             mname = each_name.text
+#             # print(mname)
+#             name.append(mname)
+
+#         price = []
+#         for each_price in soup.findAll('span',{'class':'_89yzn'}):
+#             mprice = each_price.text
+#             sprice = mprice.split()
+#             co = sprice[1].replace(",","")
+#             # print(co)
+#             price.append(co)
+#         link = []
+#         for each_img in soup.findAll('img',{'class':'_3Kg_w'}):
+#             mlink = each_img['src']
+#             link.append(mlink)
+#             # print(mlink)
+#         dt = dict(zip(name,price))
+#         # print(dt)
+#         # print(dt)
+#         price = 0
+#         mincount = 0
+#         maxcount = 0
+#         minprice = minpr(price)
+#         maxprice = 0
+#         minname = ""
+#         maxname = ""
+#         for k,v in dt.items():
+#             if int(v) < minprice:
+#                 minprice = int(v)
+#                 minname = k
+#                 mincount +=1
+
+#         for k,v in dt.items():
+#             if int(v) > maxprice:
+#                 maxprice = int(v)
+#                 maxname = k
+#                 maxcount +=1
+
+#         minsrc = link[mincount]
+#         maxsrc = link[maxcount]
+#         return render(request,"min.html",context={'html':html,'min_name':minname,'min_price':minprice,'max_name':maxname,'max_price':maxprice,'minsrc':minsrc,'maxsrc':maxsrc})
+#     except Exception as e:
+#         return render(request,"notfound.html",{'error':e})
 
 def minpr(d):
     if d == 0:
