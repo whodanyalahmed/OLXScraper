e5aeb638bcf97e8eb9fff80a1358946ef5d26145
Daniyal Ahmed <daniahmedkhatri@gmail.com>
2020-02-29 12:48:12 +0500

    debug off

--- hasnain/settings.py
+++ hasnain/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e8$yd-h5qw_acpl$ixf0t(@=n6q7x#poj(0%)e0gj6dert7jh='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
