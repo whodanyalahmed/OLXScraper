111b09678a3d49a6bd28aba126626f6aaceb4196
Daniyal Ahmed <daniahmedkhatri@gmail.com>
2020-03-09 01:03:27 +0500

    male female database setup

--- hasnain/settings.py
+++ hasnain/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
     },
 
     'replica' : {
-        'ENGINE': 'django.db.backends.sqllite3',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
 }
