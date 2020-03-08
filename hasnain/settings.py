328b13ed2bf6db648e39040899b555371fe4b8c8
Daniyal Ahmed <daniahmedkhatri@gmail.com>
2020-03-08 17:34:07 +0500

    checking configuration

--- hasnain/settings.py
+++ hasnain/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'e8$yd-h5qw_acpl$ixf0t(@=n6q7x#poj(0%)e0gj6dert7jh='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://olxscrapeer.herokuapp.com/","*"]
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1',"http://olxscrapeer.herokuapp.com"]
 
 
 # Application definition
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hasnain.urls'
@@ -85,6 +87,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
