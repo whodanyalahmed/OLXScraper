922359acc1edba2d31cf21aa36562de9c4974ee8
Daniyal Ahmed <daniahmedkhatri@gmail.com>
2020-03-18 17:17:18 +0500

    responsive

--- hasnain/settings.py
+++ hasnain/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'e8$yd-h5qw_acpl$ixf0t(@=n6q7x#poj(0%)e0gj6dert7jh='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://olxscrapeer.herokuapp.com/','localhost', '127.0.0.1']
 # ALLOWED_HOSTS = []
